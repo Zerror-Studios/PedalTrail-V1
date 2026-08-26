1dbc9e400573a4b7d26938441474243a477ede9f
inderjit-zerror <inderjit.zerror@gmail.com>
2026-08-26 14:55:00 +0530

    done

--- src/app/the-trail/page.jsx
+++ src/app/the-trail/page.jsx
@@ -7,18 +7,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
-      <TTHero/>
-      <EventOverview />
-
-      <div className='w-full h-fit px-10 py-20 relative overflow-hidden'>
-        
-        <div className='w-full h-full absolute top-0 left-0 overflow-hidden -z-1'>
-            <img src="/images/the-trail/TH2.png" alt="IMG" className='w-full h-full object-cover object-center' />
-        </div>
-
-      <EventCalendar />
-      <InvitationBanner />
-      </div>
+      
     </>
   )
 }
